095355dea17dc2c1fb734564f56a193dda203381
Arndt H. Ziegler <arndt.ziegler@infinibu.de>
2021-05-24 16:47:24 +0200

    rework while-each replacement script

--- scripts/src/while_each_to_foreach.sh
+++ scripts/src/while_each_to_foreach.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# Copyright (c) 2020 Arndt H. Ziegler <code@arndtziegler.de>
 
 VARIABLE_NAME="[^, ]*"
 
-for fl in $@; do
-	# Ersetzt while ( list($k,$v) = each($arr) ) mit foreach ( $arr as $k => $v )
-	sed -i "s/while\(\s*\)(\s*list\s*(\s*\($VARIABLE_NAME\)\s*,\s*\($VARIABLE_NAME\)\(\s*\))\s*=\s*@*each\s*(\(\s*\)\($VARIABLE_NAME\)\s*)\s*)/foreach\1(\5\6 as \2 => \3\4)/g w /dev/stdout" $fl
+for phpFile in "$@"; do
+	# Replace while( list($k,$v) = each($arr) ) with foreach( $arr as $k => $v )
+	sed -i "s/while\(\s*\)(\s*list\s*(\s*\($VARIABLE_NAME\)\s*,\s*\($VARIABLE_NAME\)\(\s*\))\s*=\s*@*each\s*(\(\s*\)\($VARIABLE_NAME\)\s*)\s*)/foreach\1(\5\6 as \2 => \3\4)/g w /dev/stdout" "$phpFile"
 	
-	# Korrektur, wenn kein Key angegeben
-	sed -i "s/foreach\(\s*\)(\(\s*\)\($VARIABLE_NAME\) as\s*=>\s*\($VARIABLE_NAME\)\(\s*\))/foreach\1(\2\3 as \4\5)/g w /dev/stdout" $fl
+	# Correct when no key is used
+	sed -i "s/foreach\(\s*\)(\(\s*\)\($VARIABLE_NAME\) as\s*=>\s*\($VARIABLE_NAME\)\(\s*\))/foreach\1(\2\3 as \4\5)/g w /dev/stdout" "$phpFile"
 done
